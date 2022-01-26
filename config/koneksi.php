51b73600106207009d08014aa955f4a6dccc2518
TIYA UTTARI <76785044+tiyauttari@users.noreply.github.com>
2022-01-26 23:11:35 +0700

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,5 @@
+<?php
+// mysqli_connect("hostname","user database","password","nama")
+$koneksi= mysqli_connect("localhost","root","","db_service") or die("Tidak bisa terhubungan ke database");
+
+
